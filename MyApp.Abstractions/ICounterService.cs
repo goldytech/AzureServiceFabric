4ea84352b65a7b58e0393c01ddd9c96bfb88ed65
Muhammad afzal Qureshi <afqureshi@apttus.com>
2018-02-05 17:56:24 +0530

    Add project files.

---
+++ MyApp.Abstractions/ICounterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Abstractions
+{
+    using Microsoft.ServiceFabric.Services.Remoting;
+
+    public interface ICounterService : IService
+    {
+        Task<long> GetCountAsync();
+
+    }
+}
---
+++ MyApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Web.Models;
+
+namespace MyApp.Web.Controllers
+{
+    using System.Fabric;
+
+    using Microsoft.ServiceFabric.Services.Client;
+    using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+    using MyApp.Abstractions;
+
+    public class HomeController : Controller
+    {
+        private readonly FabricClient fabricClient;
+
+        public HomeController(FabricClient fabricClient)
+        {
+            this.fabricClient = fabricClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var serviceUri = new Uri("fabric:/MyApp/MyApp.Counter");
+            var model = new Dictionary<Guid, long>();
+            foreach (var partition in await this.fabricClient.QueryManager.GetPartitionListAsync(serviceUri).ConfigureAwait(false))
+            {
+                var partitionKey = new ServicePartitionKey(((Int64RangePartitionInformation)partition.PartitionInformation).LowKey);
+                var proxy = ServiceProxy.Create<ICounterService>(serviceUri, partitionKey);
+                var count = await proxy.GetCountAsync().ConfigureAwait(false);
+                model.Add(partition.PartitionInformation.Id, count);
+            }
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
